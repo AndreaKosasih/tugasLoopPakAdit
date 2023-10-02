060ea770b83c5b33f8b28bffec6a60a99beed7a9
AndreaKosasih <134264397+AndreaKosasih@users.noreply.github.com>
2023-10-02 13:30:07 +0700

    Update tugas3.cpp

--- tugas3.cpp
+++ tugas3.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main() {
-    int n; cin >> n;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(j == i || j == n + 1 - i) {
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= 5; j++) {
+            if(j == i || j + i == 6) {
                 cout << "*";
             } else {
                 cout << " ";
